a5326c727bc3b606b17c14eaf869a600dd9f313d
bentaylor <63203762+BenHiramTaylor@users.noreply.github.com>
2020-09-05 12:16:19 +0100

    Added Base Logic, predicts % chance of direction

--- main.py
+++ main.py
@@ -1,8 +1,78 @@
 from Poloniex import Poloniex
+import datetime as dt
+import json
+import os
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+
+def parse_prediction_results(dic):
+    if amount_of_predictions % 100 != 0:
+        raise Exception(f"amount_of_predictions is not a multiple of 100 it is: {amount_of_predictions}")
+    
+    scaler = 0
+    num = float(0)
+    
+    if amount_of_predictions != 100:
+        scaler = amount_of_predictions / 100
+        
+    if len(dic["Higher"]) > len(dic["Lower"]):
+        direction = "Higher"
+    else:
+        direction = "Lower"
+
+    for i in dic[direction]:
+            num += i
+        
+    percentage = num/scaler
+
+    return direction, percentage
 
 if __name__ == "__main__":
-    API_Secret = ""
-    API_Key = ""
+    # GENERATE CONFIGS AND DEFAULT SETTINGS 
+    with open('APISettings.json','r') as f:
+        config = json.load(f)
+    API_Secret = config["API_Secret"]
+    API_Key = config["API_Key"]
     Polo = Poloniex(API_Key,API_Secret)
-    btc_usd = Polo.execute_command('returnTicker')['USDC_BTC']
-    print(btc_usd)
+    if not os.path.exists("CSVS"):
+        os.mkdir("CSVS")
+    interval = 86400
+    ticker = "USDT_BTC"
+    amount_of_predictions = 10000 # NEEDS TO BE MULTIPLE OF 100
+    prediction_results = {"Higher":[],"Lower":[]}
+
+    # CREATE DF AND DUMP TO CSV
+    df = Polo.create_df(ticker,interval, dt.datetime(2018,1,1))
+    df.drop(["high","low","open","volume","quoteVolume","weightedAverage"],axis=1,inplace=True)
+    df.to_csv(f"CSVS/{ticker}_{interval}.csv")
+    df["prediction"] = df["close"].shift(-1)
+    df.dropna(inplace=True)
+
+    # TRAIN THE DATA 100 TIMES TO GET %
+    for i in range(amount_of_predictions):
+        # MANIPLULATE DATA FOR TRAINING
+        future_forecast_time = 1
+        x = np.array(df.drop(["prediction"], 1))
+        y = np.array(df["prediction"])
+        x = preprocessing.scale(x)
+        x_prediction = x[-future_forecast_time:]
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
+
+        # REGRESS THE TRAINING DATA
+        clf = LinearRegression()
+        clf.fit(x_train, y_train)
+        prediction = (clf.predict(x_prediction))
+        last_close = df.tail(1)['close'].item()
+        
+        # LOG PREDICTIONS BASED ON LAST ROW
+        if prediction[0] > last_close:
+            prediction_results["Higher"].append(1)
+        else:
+            prediction_results['Lower'].append(1)
+    
+    # Calc % chance of lower/higher
+    direction, percentage = parse_prediction_results(prediction_results)
+    print(f"Predictions have calculated that there is a {percentage}% chance of the price being {direction}.")
+    
