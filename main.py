8bb5f37cebbf3d5a359ce529129890469ad86a74
Ben Taylor <63203762+BenHiramTaylor@users.noreply.github.com>
2020-09-12 16:31:57 +0100

    changed from csv to json, logging prediction and close later on

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def parse_prediction_results(dic):
     
     if amount_of_predictions != 100:
         scaler = amount_of_predictions / 100
-        
+
     if len(dic["Higher"]) > len(dic["Lower"]):
         direction = "Higher"
     else:
@@ -38,8 +38,8 @@ if __name__ == "__main__":
     API_Secret = config["API_Secret"]
     API_Key = config["API_Key"]
     Polo = Poloniex(API_Key,API_Secret)
-    if not os.path.exists("CSVS"):
-        os.mkdir("CSVS")
+    if not os.path.exists("JSON"):
+        os.mkdir("JSON")
     interval = 86400
     ticker = "USDT_BTC"
     amount_of_predictions = 10000 # NEEDS TO BE MULTIPLE OF 100
@@ -48,10 +48,26 @@ if __name__ == "__main__":
     # CREATE DF AND DUMP TO CSV
     df = Polo.create_df(ticker,interval, dt.datetime(2018,1,1))
     df.drop(["high","low","open","volume","quoteVolume","weightedAverage"],axis=1,inplace=True)
-    df.to_csv(f"CSVS/{ticker}_{interval}.csv")
     next_interval = dt.datetime.strptime(df.tail(1).index.item(), "%Y-%m-%d %H:%M:%S") + dt.timedelta(seconds=interval)
     df["prediction"] = df["close"].shift(-1)
     df.dropna(inplace=True)
+    json_string = df.to_json(orient="index")
+    new_json_data = json.loads(json_string)
+
+    if not os.path.exists(f"JSON\\{ticker}_{interval}_log.json"):
+        with open(f"JSON\\{ticker}_{interval}_log.json","w")as f:
+            json.dump(new_json_data,f,indent=2)
+
+    with open(f"JSON\\{ticker}_{interval}_log.json","r") as f:
+        json_file = json.load(f)
+    
+    for k in new_json_data:
+        if k in json_file:
+            for key in new_json_data[k]:
+                if key == "LRPrediction":
+                    continue
+                if new_json_data[k][key] != json_file[k][key]:
+                    json_file[k][key] = new_json_data[k][key]
 
     current_price = Polo.get_current_price(ticker)
 
@@ -82,4 +98,9 @@ if __name__ == "__main__":
     # Calc % chance of lower/higher
     direction, percentage, average = parse_prediction_results(prediction_results)
     print(f"Predictions have calculated that there is a {percentage}% chance of the price being {direction} than the current price of: {current_price} at the next interval of: {next_interval}.\nAverage price predicted: {average}")
+    
+    json_file[dt.datetime.strftime(next_interval,"%Y-%m-%d %H:%M:%S")] = {"close":"","prediction":"","LRPrediction":average}
+
+    with open(f"JSON\\{ticker}_{interval}_log.json","w")as f:
+        json.dump(json_file,f,indent=2)
     
