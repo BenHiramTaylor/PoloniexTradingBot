acc4261da8988291f6b51cd7321073429812dcfa
bentaylor <63203762+BenHiramTaylor@users.noreply.github.com>
2020-09-13 21:41:16 +0100

    Only refresh open_positions once unless changed

--- main.py
+++ main.py
@@ -104,8 +104,8 @@ if __name__ == "__main__":
                         print(f"Killing {open_positions[t][p]['type']} order with order number {order_number}\nPosition was opened on: {open_positions[t][p]['date']}")
                         Polo.api_query("cancelOrder",{"orderNumber":order_number})
 
-        # REFRESH ALL OPEN POSITIONS AFTER KILLING OLD ONES
-        open_positions = Polo.load_all_open_positions()
+            # REFRESH ALL OPEN POSITIONS AFTER KILLING OLD ONES
+            open_positions = Polo.load_all_open_positions()
  
         # CREATE DF AND DUMP TO CSV
         df = Polo.auto_create_df(ticker,interval)
